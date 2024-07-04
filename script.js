9d8a441cdb0143071d838120def27b6a09982b4c
tigricina <karina6891@mail.ru>
2024-07-04 03:47:01 +0300

    add value to variables and get some operations with them

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
-alert("Hello, dear kitten!");
-console.log("Hello, dear puppy!");
+let title = "js_long_may_2024_GLO",
+  screens = "Простые, Сложные, Интерактивные",
+  screenPrice = 10500,
+  rollback = 75,
+  fullPrice = 500000,
+  adaptive = true;
+
+let array = screens.toLowerCase().split(",");
+
+console.log(typeof title, fullPrice, adaptive);
+console.log(screens.length);
+console.log(
+  "Стоимость верстки экранов " +
+    screenPrice +
+    " рублей. Стоимость разработки сайта " +
+    fullPrice +
+    " рублей"
+);
+console.log(array);
+console.log(fullPrice * (rollback / 100));
+
+/*
+2) Используя методы и свойства:
+
+    Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
+    Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100)) */
